f7a860cda5d6f8b6a4a833e981f219770a85cc44
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-06 01:52:25 +0200

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ChannelDetail from "./components/ChannelDetail";
+import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
+import SearchFeed from "./components/SearchFeed";
+import VideoDetail from "./components/VideoDetail";
 
 const App = () => {
-  return <div>AppAppAppAppAppApp</div>;
-}
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" exact element={<VideoDetail />} />
+          <Route path="/channel/:id" exact element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
