7f7114325d7d052b5b3f7ac42ed16e710964b071
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-06 02:12:46 +0200

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@
 import { Box } from "@mui/material";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ChannelDetail from "./components/ChannelDetail";
-import Feed from "./components/Feed";
-import Navbar from "./components/Navbar";
-import SearchFeed from "./components/SearchFeed";
-import VideoDetail from "./components/VideoDetail";
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Box sx={{ backgroundColor: "#000" }}>
+      <Box sx={{ backgroundColor: "#000", color:"#fff", width: "100%" }}>
         <Navbar />
         <Routes>
           <Route path="/" exact element={<Feed />} />
